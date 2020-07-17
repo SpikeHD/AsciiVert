2fc1cab29c88d692d6f27cde328400215cd407fc
spikegd <spikegdofficial@gmail.com>
2020-07-16 18:56:19 -0700

    Skeleton functions and comment improvements

--- processor/image.js
+++ processor/image.js
@@ -6,10 +6,11 @@ const symbols = ['@', '#', '$', '%', ';', ':', '^', '*', ',', '.', '\'', ' ']
  * Convert a single image file to ascii art.
  * 
  * @param {String} infile 
- * @param {String} outfile 
+ *  Path to image file.
  * @param {Object} resolution 
+ *  Desired resolution.
  */
-exports.imageToAscii = (infile, resolution) => {
+exports.imageToText = (infile, resolution) => {
   return new Promise(async (resolve, reject) => {
     jimp.read(infile, (err, img) => {
       if(err) reject(err)
@@ -33,6 +34,8 @@ exports.imageToAscii = (infile, resolution) => {
           if(x == resolution.width) str += row + '\n'
         }
 
+        // When we finish, we can return some usefull data
+        // as well as the raw string
         if(y == resolution.height) {
           resolve({
             width: resolution.width,
@@ -45,6 +48,36 @@ exports.imageToAscii = (infile, resolution) => {
   })
 }
 
+/**
+ * Convert a string of text to an image file.
+ * 
+ * @param {String} text 
+ *  Text to be made into image.
+ * @param {String} outfile 
+ *  Path to the finished file.
+ */
+exports.textToImage = async (text, outfile) => {
+
+}
+
+/**
+ * 
+ * @param {String} filename 
+ *  File to write.
+ * @param {String} img 
+ *  Image to write.
+ */
+function writebuf(filename, img) {
+
+}
+
+/**
+ * 
+ * @param {Object} color 
+ *  RGB color object.
+ * @param {Array} symbols 
+ *  Array of available symbols.
+ */
 function calculateSymbol(color, symbols) {
   // https://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx
   let brightness = Math.sqrt(
