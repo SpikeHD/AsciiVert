5d4e93026939857268bdb1af78eb06fd61799733
spikegd <spikegdofficial@gmail.com>
2020-07-20 18:31:05 -0700

    Button styling

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -3,6 +3,11 @@ function menuHandle(item) {
   $(`.${item}`).slideToggle('fast')
 }
 
+/**
+ * Submit image file, return image in ascii form.
+ * 
+ * @param {String} inputId 
+ */
 function ajaxSubmitImage(inputId) {
   // Get file from file select
   var file = document.getElementById(inputId).files[0]
@@ -21,12 +26,19 @@ function ajaxSubmitImage(inputId) {
   // Send request
   xhr.open('POST', '/image', true)
   xhr.onreadystatechange = function() {
-    $('.image_ascii_result').prop('href', `/file?id=${xhr.responseText}`)
-    $('.image_ascii_result').text("Here's your file!")
+    if(xhr.status === 200) {
+      $('.image_ascii_result').prop('href', `/file?id=${xhr.responseText}`)
+      $('.image_ascii_result').text("Here's your file!")
+    }
   }
   xhr.send(formData)
 }
 
+/**
+ * Submit image file, return image text.
+ * 
+ * @param {String} inputId 
+ */
 function ajaxSubmitMini(inputId) {
   // Get file from file select
   var file = document.getElementById(inputId).files[0]
@@ -45,7 +57,41 @@ function ajaxSubmitMini(inputId) {
   // Send request
   xhr.open('POST', '/mini', true)
   xhr.onreadystatechange = function () {
-    $('.image_mini_result').text(JSON.parse(xhr.responseText).content)
+    if(xhr.status === 200) {
+      $('.image_mini_result').text(JSON.parse(xhr.responseText).content)
+    }
+  }
+  xhr.send(formData)
+}
+
+/**
+ * Submit video file, return ascii video link.
+ * 
+ * @param {String} inputId 
+ */
+function ajaxSubmitMini(inputId) {
+  // Get file from file select
+  var file = document.getElementById(inputId).files[0]
+  var resolution = {
+    width: $(`#${inputId}`).parent().find(`#image_width`).val(),
+    height: $(`#${inputId}`).parent().find(`#image_height`).val()
+  }
+  var framerate = $(`#${inputId}`).parent().find(`#framerate`).val()
+
+  // FormData object
+  var formData = new FormData()
+  formData.append('files', file, file.name)
+  formData.append('resolution', JSON.stringify(resolution))
+  formData.append('framerate', framerate)
+
+  var xhr = new XMLHttpRequest()
+
+  // Send request
+  xhr.open('POST', '/mini', true)
+  xhr.onreadystatechange = function () {
+    if(xhr.status === 200) {
+
+    }
   }
   xhr.send(formData)
 }
