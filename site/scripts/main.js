7978f7c4473747c594ea9f5a95fbba70d9d003fe
spikegd <spikegdofficial@gmail.com>
2020-07-20 19:52:42 -0700

    Styling changes 2

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -3,6 +3,14 @@ function menuHandle(item) {
   $(`.${item}`).slideToggle('fast')
 }
 
+function displayFilename(e) {
+  // Get file from file select
+  var file = e.target.files[0]
+  var name = file.name
+
+  $(`#${e.target.id}`).parent().find('.label_inner').text(name)
+}
+
 /**
  * Submit image file, return image in ascii form.
  * 
@@ -27,8 +35,10 @@ function ajaxSubmitImage(inputId) {
   xhr.open('POST', '/image', true)
   xhr.onreadystatechange = function() {
     if(xhr.status === 200) {
-      $('.image_ascii_result').prop('href', `/file?id=${xhr.responseText}`)
-      $('.image_ascii_result').text("Here's your file!")
+      let result = $('.image_ascii_result')
+      result.prop('src', `/file?id=${xhr.responseText}`)
+      result.css('width', '100%')
+      result.css('border', '1px solid #000')
     }
   }
   xhr.send(formData)
@@ -69,7 +79,7 @@ function ajaxSubmitMini(inputId) {
  * 
  * @param {String} inputId 
  */
-function ajaxSubmitMini(inputId) {
+function ajaxSubmitVideo(inputId) {
   // Get file from file select
   var file = document.getElementById(inputId).files[0]
   var resolution = {
